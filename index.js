018c08704339b699217848078c5c7138a824dd53
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-18 00:46:44 +0100

    Exercise 3.11 phonebook full stack: add the frontend build to the backend application

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(express.json())
 morgan.token('data',  (req, res) => req.method === 'POST' ? JSON.stringify(req.body): ' ' )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('dist'))
 
 let persons = [
     {
