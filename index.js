cd1c82929e5c02a7fd61bb170053b4623a614405
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-20 00:57:15 +0100

    Exercise 3.13: Phonebook database, step1: fetch phonebook entries from the Mongo database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 app.use(cors())
@@ -9,31 +12,11 @@ morgan.token('data',  (req, res) => req.method === 'POST' ? JSON.stringify(req.b
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.static('dist'))
 
-let persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -83,7 +66,7 @@ app.get('/info', (request, response) => {
     response.send(info)
   })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
