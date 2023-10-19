5ac4bbf4edcf446ff71f94358dd2e3895e7da563
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-20 00:29:03 +0100

    Exercise 3.12: Command-line database: create a command-line mongoose database utility

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('You need to specify the password at least')
+  process.exit(1)
+}
+
+let name, number
+const password = process.argv[2]
+if (process.argv.length > 3) {
+    name = process.argv[3]
+    number = process.argv[4]
+}
+
+const url =
+  `mongodb+srv://fullstackopen:${password}@bit2cluster.qcfygi7.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+
+if (name) {
+    const person = new Person({name, number})
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
