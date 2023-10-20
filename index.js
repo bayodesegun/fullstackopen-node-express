cba8786da983d107356f05a90694311c32a2020a
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-20 19:28:49 +0100

    Exercise 3.16: Phonebook database, step4: add error handling separate module

--- index.js
+++ index.js
@@ -13,58 +13,68 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 app.use(express.static('dist'))
 
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(result => {
-    response.json(result)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+		.then(result => {
+			response.json(result)
+		})
+		.catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const person = request.body
-    let error = 'Please fix the following errors: '
-    if (!person.name) {
-        error = `${error}
-- Name is missing`
-    }
-    if (!person.number) {
-        error = `${error}
-- Number is missing`
-    }
-    if (error.includes('\n'))
-        return response.status(400).json({error})
+app.post('/api/persons', (request, response, next) => {
+	const person = request.body
+	let error = {}
+	if (!person.name) error.name = 'Name is missing'
+	if (!person.number) error.number = 'Number is missing'
+	if (Object.keys(error).length > 0) {
+		return response.status(400).json({error})
+	}
 
-    const {name, number} = person
-    const newPerson = new Person({name, number})
-    newPerson.save().then(result => {
-      console.log(`Created a new entry - ${result.name} ${result.number}`)
-      response.status(201).json(result)
-    })
+	const {name, number} = person
+	const newPerson = new Person({name, number})
+	newPerson.save()
+		.then(result => {
+			console.log(`Created a new entry - ${result.name} ${result.number}`)
+			response.status(201).json(result)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(_person => _person.id === id)
-    if (!person) return response.status(404).json({detail: 'Phonebook entry not found!'})
+	const id = Number(request.params.id)
+	const person = persons.find(_person => _person.id === id)
+	if (!person) return response.status(404).json({detail: 'Phonebook entry not found!'})
 
-    response.json(person)
+	response.json(person)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id).then(result => {
-      console.log(`Deleted ${result.name} (${result.number}) from the phonebook.`)
-      response.status(204).end()
-    })
+app.delete('/api/persons/:id', (request, response, next) => {
+	const id = request.params.id
+	Person.findByIdAndDelete(id)
+		.then(result => {
+			if (result)
+				console.log(`Deleted ${result.name} (${result.number}) from the phonebook.`)
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    const info = `
-    <p>Phonebook has info for ${persons.length} ${persons.length === 1 ? 'person' : 'people'}</p>
-    <p>${new Date()}</p>
-    `
-    response.send(info)
-  })
+	const info = `
+	<p>Phonebook has info for ${persons.length} ${persons.length === 1 ? 'person' : 'people'}</p>
+	<p>${new Date()}</p>
+	`
+	response.send(info)
+})
 
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
