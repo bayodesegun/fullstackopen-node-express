1cdc368ce576015bf4a07173f1f827a6b07622f1
Bayode Aderinola <bayodesegun@gmail.com>
2023-10-23 00:55:50 +0100

    Example 3.20*: Phonebook database, step8: add db schema validation for phone number

--- models/person.js
+++ models/person.js
@@ -20,7 +20,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
   }
 })
 
